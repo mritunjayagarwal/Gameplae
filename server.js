6af283ccef9b09b5a07ef1000a6113271bc1abfe
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2020-05-14 15:13:59 +0530

    Removed Node_modules glitch

--- server.js
+++ server.js
@@ -7,13 +7,15 @@ const MongoStore = require('connect-mongo')(session);
 const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const passport = require('passport');
+const compression = require('compression');
+const helmet = require('helmet');
 const http = require('http');
 const container = require('./container');
 
 container.resolve(function(users, tournament, _){
 
     mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://bigmoney:Zoniackk3@ds251618.mlab.com:51618/slingshot', { useNewUrlParser: true, useUnifiedTopology: true})
+    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
 
     const app = ShowExpress();
 
@@ -24,7 +26,7 @@ container.resolve(function(users, tournament, _){
 
         configureExpress(app);
 
-        server.listen(port, function(err){
+        server.listen(process.env.PORT || port, function(err){
             if(err) console.log(err);
             console.log("SlingShot is Up and Running");
         });
@@ -34,9 +36,17 @@ container.resolve(function(users, tournament, _){
         tournament.SetRouting(router);
 
         app.use(router);
+
+        app.use(function(req, res){
+            res.render('404');
+        })
+
     }
 
     function configureExpress(app){
+
+        app.use(compression());
+        app.use(helmet());
 
         require('./passport/signup');
         require('./passport/login');
@@ -48,7 +58,7 @@ container.resolve(function(users, tournament, _){
         app.use(session({
             resave: true,
             saveUninitialized: true,
-            secret: 'mommy',
+            secret: process.env.SECRET_KEY,
             store: new MongoStore({ mongooseConnection: mongoose.connection})
         }));
         app.use(flash());        
