569851755d3f3db2fbb1af83aa58904e8a0c5e57
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2020-07-18 23:18:20 +0530

    UPI wallet

--- public/js/index.js
+++ public/js/index.js
@@ -4,4 +4,13 @@ $("#upi").on("change", function() {
     $('#upiNum').toggle();
   });
 
+$("#dUpi").on("click", function(){
+  $('#newUpi').toggle();
+  $('#preUpi').toggle();
+  $('#sbtn').toggle();
+  $('#dUpi').toggleClass('mb-0');
+  $(this).text($('#dUpi').text() == 'Use Another UPI' ? 'Use a registered UPI' : 'Use Another UPI');
+  
+})
+
   
